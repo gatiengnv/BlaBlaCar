fd45d92fb451b24f3fcba24303b8e1b1d71ab622
nathannct <nathan.nicart@utt.fr>
2026-06-03 00:26:27 +0200

    Add router PHPDoc

--- app/router/router.php
+++ app/router/router.php
@@ -1,29 +1,37 @@
-
-<!-- ----- debut Router -->
 <?php
-require ('../controller/ControllerMenu.php');
 
-// --- récupération de l'action passée dans l'URL
+require('../controller/ControllerMenu.php');
+require('../controller/ControllerAdministrateur.php');
+
+/** récupération de l'action passée dans l'URL */
 $query_string = $_SERVER['QUERY_STRING'];
 
-// fonction parse_str permet de construire 
-// une table de hachage (clé + valeur)
+/** 
+ * Permet de construire une table de hachage (clé + valeur) 
+ */
 parse_str((string)($_SERVER['QUERY_STRING'] ?? ''), $param);
 $action = htmlspecialchars($_POST['action'] ?? ($param['action'] ?? ''));
 
-// --- Préparation de la structure $args contenant tous les paramètres
-// Fusion des paramètres GET et POST
+/**
+ * Préparation de la structure $args contenant tous les paramètres
+ * Fusion des paramètres GET et POST
+ */
 $args = array_merge($param, $_POST);
-// Suppression de la clé 'action' des paramètres pour éviter les doublons
+
+/** Suppression de la clé 'action' des paramètres pour éviter les doublons */
 unset($args['action']);
 
-// --- Liste blanche des actions autorisees
+/** Liste blanche des actions autorisees */
 $allowed_actions = array(
     // Menu
     "menuAccueil" => array("controller" => "ControllerMenu", "method" => "menuAccueil"),
+
+    // Administateur
+    "utilisateursReadAll" => array("controller" => "ControllerAdministrateur", "method" => "utilisateursReadAll"),
+
 );
 
-// Tache par defaut
+/** Tache par defaut */ 
 if (!array_key_exists($action, $allowed_actions)) {
     $action = "menuAccueil";
 }
@@ -34,5 +42,3 @@ $method = $route["method"];
 
 $controller::$method($args);
 ?>
-<!-- ----- Fin Router -->
-
